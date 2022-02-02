17de69cbc34d37f43c9973071468d45adc9106a9
antonKLY <98814823+antonKLY@users.noreply.github.com>
2022-02-02 14:18:13 +0300

    Delete EmployeeAgeComparator.java

--- EmployeeAgeComparator.java
+++
@@ -1,8 +0,0 @@
-import java.util.Comparator;
-public class EmployeeAgeComparator implements Comparator<OfficeEmployee> {
-
-    @Override
-    public int compare(OfficeEmployee o1, OfficeEmployee o2) {
-        return Integer.compare(o2.getAge(), o1.getAge());
-    }
-}
